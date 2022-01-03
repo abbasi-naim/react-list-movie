d53c2a43441c1a46b2618dac95faf841de3a4cd7
abbasi-naim <abbasi.naim@gmail.com>
2022-01-04 01:17:46 +0300

    create movies class on movies component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,1 +1,13 @@
 import React, { Component } from "react";
+
+class Movies extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Movies Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Movies;
