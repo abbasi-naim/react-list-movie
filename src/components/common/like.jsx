3ef26e7d59b082fadd7bc5e1d48c6d364e428304
abbasi-naim <abbasi.naim@gmail.com>
2022-01-10 23:52:38 +0300

    change fontawsome of heart

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Like extends Component {
   render() {
-    return <i class="fa fa-heart-o" aria-hidden="true"></i>;
+    return <i class="far fa-heart"></i>;
   }
 }
 
