a5e1a4ec0fcfdd010a764f4bd25bded18be0bcbb
abbasi-naim <abbasi.naim@gmail.com>
2022-01-11 13:27:35 +0300

    added condition to like icon

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Like = () => {
-  return <i className="fa fa-heart-o" aria-hidden="true"></i>;
+  let classes = "fa fa-heart";
+  if (!this.props.liked) classes += "-o";
+  return <i className={classes} aria-hidden="true"></i>;
 };
 
 export default Like;
