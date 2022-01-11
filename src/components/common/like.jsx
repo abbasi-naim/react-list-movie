e91e6a8b0d5e07400a1126d6092a7a148b983b2b
abbasi-naim <abbasi.naim@gmail.com>
2022-01-11 15:22:18 +0300

    added props as arg on like component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Like = () => {
+const Like = (props) => {
   let classes = "fa fa-heart";
-  if (!this.props.liked) classes += "-o";
+  if (!props.liked) classes += "-o";
   return (
     <i
-      onClick={this.props.onClick}
+      onClick={props.onClick}
       className={classes}
       style={{ cursor: "pointer" }}
       aria-hidden="true"
