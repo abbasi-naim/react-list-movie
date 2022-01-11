092371f81436fc48e1d430156048d1a576f69b85
abbasi-naim <abbasi.naim@gmail.com>
2022-01-11 13:23:47 +0300

    changed like.jsx to stateless function

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Like extends Component {
-  render() {
-    return <i className="fa fa-heart-o" aria-hidden="true"></i>;
-  }
-}
+const Like = () => {
+  return <i className="fa fa-heart-o" aria-hidden="true"></i>;
+};
 
 export default Like;
