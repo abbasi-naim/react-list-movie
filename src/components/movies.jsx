b72c96c8240c87651f07906c83019771950eb25f
abbasi-naim <abbasi.naim@gmail.com>
2022-01-14 21:35:34 +0300

    added condition if there is one page dont show 1

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -73,7 +73,7 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={10}
           onPageChange={this.handlePageChange}
         />
       </div>
